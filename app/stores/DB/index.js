0a7188c33a2756341e8756786ab1befeaf391eed
wasd171 <wasd171@gmail.com>
2017-01-21 18:09:32 +0100

    Added proper toJS convert for db

--- app/stores/DB/index.js
+++ app/stores/DB/index.js
@@ -2,6 +2,7 @@ import defaultDB from './db'
 import PromiseWorker from 'promise-worker'
 import Worker from 'worker-loader?inline!babel-loader!./worker.js'
 import {WORKER_SET_MATCHES, WORKER_GET_MATCHES, WORKER_SET_PROFILE, WORKER_SET_PERSON, WORKER_GET_INITIAL} from 'app/constants'
+import {toJS} from 'mobx'
 
 
 export class DB {
@@ -15,23 +16,23 @@ export class DB {
     }
 
     saveToDB = (table, index, data) => {
-        return this.db[table].put(data, index)
+        return this.db[table].put(toJS(data), index)
     };
 
     setMatches = (history) => {
-        return this.sendToWorker(WORKER_SET_MATCHES, history);
+        return this.sendToWorker(WORKER_SET_MATCHES, toJS(history));
     };
 
     getMatches = (matches) => {
-        return this.sendToWorker(WORKER_GET_MATCHES, matches);
+        return this.sendToWorker(WORKER_GET_MATCHES, toJS(matches));
     };
 
     saveProfile = (profile) => {
-        return this.sendToWorker(WORKER_SET_PROFILE, profile);
+        return this.sendToWorker(WORKER_SET_PROFILE, toJS(profile));
     }
 
     savePerson = (profile) => {
-        return this.sendToWorker(WORKER_SET_PERSON, profile);
+        return this.sendToWorker(WORKER_SET_PERSON, toJS(profile));
     }
 
     getInitial = () => {
