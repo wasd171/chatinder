57b0e508f75aed146ec5302ccd0d2239673ab699
Konstantin Nesterov <wasd171@gmail.com>
2017-04-24 15:46:16 +0200

    Let computed do the monkey job

--- src/main/ServerAPI/schema/resolvers/resolverMap.js
+++ src/main/ServerAPI/schema/resolvers/resolverMap.js
@@ -1,27 +1,11 @@
 // @flow
-import {isAuthorized, matches, profile, match} from './queries'
-import {login, showWindow, logout, sendMessage, updatePerson, checkDoMatchesExist, subscribeToUpdates} from './mutations'
-import {Profile, Person, Match, Message} from './instances'
+import * as Query from './queries'
+import * as Mutation from './mutations'
+import * as Instances from './instances'
 
 
 export const resolverMap = {
-    Query: {
-        isAuthorized,
-        matches,
-        profile,
-        match
-    },
-    Mutation: {
-        login,
-        showWindow,
-        logout,
-        sendMessage,
-        updatePerson,
-        checkDoMatchesExist,
-        subscribeToUpdates
-    },
-    Profile,
-    Person,
-    Match,
-    Message
+    Query,
+    Mutation,
+    ...Instances
 }
