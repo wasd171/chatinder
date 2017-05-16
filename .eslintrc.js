ec84bf2f8ae0fd8a5d3fa7545a9375bf8bc78d73
Konstantin Nesterov <wasd171@gmail.com>
2017-05-16 17:18:33 +0200

    Installed deps and added eslint as precommit script

---
+++ .eslintrc.js
@@ -0,0 +1,29 @@
+module.exports = {
+    extends: [
+        'prettier'
+    ],
+    env: {
+        es6: true,
+        browser: true,
+        node: true
+    },
+    parser: 'babel-eslint',
+    parserOptions: {
+        ecmaVersion: 2017,
+        sourceType: 'module',
+        ecmaFeatures: {
+            impliedStrict: true
+        }
+    },
+    plugins: [
+        'prettier'
+    ],
+    rules: {
+        'prettier/prettier': ['error', {
+            useTabs: true,
+            tabWidth: 4,
+            singleQuote: true,
+            semi: false
+        }]
+    }
+}
