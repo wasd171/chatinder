cb02b932b5881c7057ac6e29f6cd66fac910e1df
Konstantin Nesterov <wasd171@gmail.com>
2017-05-22 22:48:29 +0200

    Last VIEW_MAIN -> VIEW_MATCHES transform

--- src/shared/utils/nameToPath.js
+++ src/shared/utils/nameToPath.js
@@ -1,12 +1,17 @@
 // @flow
-import { VIEW_CHAT, VIEW_USER, VIEW_LOADING, VIEW_MAIN } from 'shared/constants'
+import {
+	VIEW_CHAT,
+	VIEW_USER,
+	VIEW_LOADING,
+	VIEW_MATCHES
+} from 'shared/constants'
 
 export function nameToPath(name: string, param: string | void) {
 	switch (name) {
 		case VIEW_CHAT:
-			return `${nameToPath(VIEW_MAIN)}/${param || ':id'}/${VIEW_CHAT}`
+			return `${nameToPath(VIEW_MATCHES)}/${param || ':id'}/${VIEW_CHAT}`
 		case VIEW_USER:
-			return `${nameToPath(VIEW_MAIN)}/${param || ':id'}/${VIEW_USER}`
+			return `${nameToPath(VIEW_MATCHES)}/${param || ':id'}/${VIEW_USER}`
 		case VIEW_LOADING:
 			return `/${VIEW_LOADING}/${param || ':title'}`
 		default:
