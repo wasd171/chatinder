0ef3f9d85a3ca029c5e738dd07d553a42da17c45
Konstantin Nesterov <wasd171@gmail.com>
2017-05-23 20:18:01 +0200

    Added back arrow in user profile

--- src/app/scenes/App/scenes/Main/scenes/UserHeader/index.jsx
+++ src/app/scenes/App/scenes/Main/scenes/UserHeader/index.jsx
@@ -1,28 +1,48 @@
 import React, { Component } from 'react'
 import muiThemeable from 'material-ui/styles/muiThemeable'
 import styled from 'styled-components'
+import NavigationArrowBack from 'material-ui/svg-icons/navigation/arrow-back'
+import { inject } from 'mobx-react'
 
 const OuterWrapper = styled.div`
 	display: flex;
-	flex-direction: column;
-	justify-content: center;
+	flex-direction: row;
+	justify-content: space-between;
 	align-items: center;
 	height: 100%;
 	width: 100%;
+	padding-left: 5px;
+	padding-right: 5px;
+`
+
+const IconWrapper = styled.span`
+	width: 17px;
+	${props => props.activated && 'cursor: pointer;'}
 `
 
 const NameSpan = styled.span`
 	color: ${props => props.theme.palette.textColor};
 `
 
+@inject('navigator')
 @muiThemeable()
 class UserHeader extends Component {
+	handleClick = () => {
+		this.props.navigator.goBack()
+	}
+
 	render() {
 		return (
 			<OuterWrapper>
+				<IconWrapper activated onClick={this.handleClick}>
+					<NavigationArrowBack
+						color={this.props.muiTheme.palette.primary1Color}
+					/>
+				</IconWrapper>
 				<NameSpan theme={this.props.muiTheme}>
 					Info
 				</NameSpan>
+				<IconWrapper />
 			</OuterWrapper>
 		)
 	}
