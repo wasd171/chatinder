c4e50297e1a86afd7fd01849624e9868152239f8
Konstantin Nesterov <wasd171@gmail.com>
2017-06-30 00:47:20 +0200

    Fixed logout error

--- src/main/ServerAPI/AppManager/logoutFactory.ts
+++ src/main/ServerAPI/AppManager/logoutFactory.ts
@@ -5,10 +5,14 @@ export default function logoutFactory(instance: AbstractAppManager) {
 	return async function logout() {
 		if (instance._window !== null) {
 			const { session } = instance._window.webContents
-			await fromCallback(callback => session.clearCache(callback))
+			await fromCallback(callback =>
+				session.clearCache(() => callback(null))
+			)
+
 			await fromCallback(callback =>
 				session.clearStorageData({}, callback)
 			)
+
 			instance._window.destroy()
 			instance._window = null
 		} else {
--- src/main/ServerAPI/schema/resolvers/mutations/logout.ts
+++ src/main/ServerAPI/schema/resolvers/mutations/logout.ts
@@ -3,22 +3,24 @@ import { fromCallback } from '~/shared/utils'
 import { AbstractServerAPI } from '~/shared/definitions'
 import { app } from 'electron'
 
-export function logout(_obj: undefined, _args: {}, ctx: AbstractServerAPI) {
-	setTimeout(async () => {
-		await Promise.all([
-			ctx.app.logout(),
-			fromCallback(callback =>
-				ctx.db.matches.remove({}, { multi: true }, callback)
-			),
-			fromCallback(callback =>
-				ctx.db.extra.remove({}, { multi: true }, callback)
-			),
-			fromCallback(callback =>
-				ctx.db.pending.remove({}, { multi: true }, callback)
-			)
-		])
-		app.relaunch()
-		app.exit(0)
-	}, 1000)
+export async function logout(
+	_obj: undefined,
+	_args: {},
+	ctx: AbstractServerAPI
+) {
+	await Promise.all([
+		ctx.app.logout(),
+		fromCallback(callback =>
+			ctx.db.matches.remove({}, { multi: true }, callback)
+		),
+		fromCallback(callback =>
+			ctx.db.extra.remove({}, { multi: true }, callback)
+		),
+		fromCallback(callback =>
+			ctx.db.pending.remove({}, { multi: true }, callback)
+		)
+	])
+	app.relaunch()
+	app.exit(0)
 	return success
 }
