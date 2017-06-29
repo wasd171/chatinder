22b2203da6dc7d092d43672514e0ef62a85d7cd1
Konstantin Nesterov <wasd171@gmail.com>
2017-06-29 22:27:02 +0200

    Fix emojione

--- src/app/shims/emojione.ts
+++ src/app/shims/emojione.ts
@@ -1,12 +1,7 @@
 import * as emojione from 'emojione'
 
-// emojione.imageType = 'png'
-// emojione.sprites = false
-// emojione.imagePathPNG = '../node_modules/emojione/assets/png/'
+emojione.imageType = 'png'
+emojione.sprites = false
+emojione.imagePathPNG = '../node_modules/emojione/assets/png/'
 
-export default {
-	...emojione,
-	imageType: 'png',
-	sprites: 'false',
-	imagePathPNG: '../node_modules/emojione/assets/png/'
-}
+export default emojione
