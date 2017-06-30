17eea6b060c5c0c12d7caf745334c8bba967b3b4
Konstantin Nesterov <wasd171@gmail.com>
2017-06-30 02:53:43 +0200

    Moved requires inside the function (for tree-shaking)

--- src/shared/utils/ravenSetupMain.ts
+++ src/shared/utils/ravenSetupMain.ts
@@ -1,6 +1,6 @@
-import * as Raven from 'raven'
-
 export function ravenSetupMain() {
+	const Raven = require('raven')
+
 	Raven.config(
 		'https://da10ea27ad724a2bbd826e378e1c389b:00489345c287416fad67161c62002221@sentry.io/183877'
 	).install()
