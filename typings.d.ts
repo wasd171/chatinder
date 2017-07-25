31c2d25c7f3c44a01a207e595d447c4f18c4ea75
Konstantin Nesterov <wasd171@gmail.com>
2017-07-25 14:09:15 +0200

    Add typings

--- typings.d.ts
+++ typings.d.ts
@@ -13,3 +13,70 @@ declare module '*.graphql' {
 declare module 'emojione/package.json' {
 	export const version: string
 }
+
+declare namespace NodeJS {
+	export interface Global {
+		jQuery: any
+		emojione: any
+		emojioneVersion: string
+		Perf: any
+	}
+}
+
+declare module 'react-image-gallery' {
+	import * as React from 'react'
+
+	export type ClickHandler = React.MouseEventHandler<HTMLButtonElement>
+	type CustomRenderer = (
+		onClick: ClickHandler,
+		disabled: boolean
+	) => JSX.Element
+
+	export interface Item {
+		original: string
+	}
+
+	interface Props {
+		items: Item[]
+		showPlayButton: boolean
+		showBullets: boolean
+		showThumbnails: boolean
+		showFullscreenButton: boolean
+		infinite: boolean
+		renderLeftNav: CustomRenderer
+		renderRightNav: CustomRenderer
+	}
+
+	class ImageGallery extends React.Component<Props> {}
+	export default ImageGallery
+}
+
+declare module 'material-ui/TextField/TextFieldHint' {
+	import * as React from 'react'
+	import { MuiTheme } from 'material-ui/styles'
+
+	interface Props {
+		muiTheme: MuiTheme
+		show: boolean
+		text: string
+	}
+
+	class TextFieldHint extends React.Component<Props> {}
+
+	export default TextFieldHint
+}
+
+declare module 'material-ui/TextField/TextFieldUnderline' {
+	import * as React from 'react'
+	import { MuiTheme } from 'material-ui/styles'
+
+	interface Props {
+		muiTheme: MuiTheme
+		disabled: boolean
+		focus: boolean
+	}
+
+	class TextFieldUnderline extends React.Component<Props> {}
+
+	export default TextFieldUnderline
+}
