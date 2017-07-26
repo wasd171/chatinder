d61dbc6a75a4daddef5935a27f6bdcc83ac73dbc
Konstantin Nesterov <wasd171@gmail.com>
2017-07-26 11:16:56 +0200

    Move all Tinder definitions to single file

--- src/shared/definitions/AbstractTinderAPIParams.ts
+++
@@ -1,6 +0,0 @@
-import { AbstractStorage } from '.'
-
-export abstract class AbstractTinderAPIParams {
-	lastActivityDate: Date
-	storage: AbstractStorage
-}
--- src/shared/definitions/AbstractTinderAPISaved.ts
+++
@@ -1,3 +0,0 @@
-export abstract class AbstractTinderAPISaved {
-	lastActivityTimestamp?: number
-}
